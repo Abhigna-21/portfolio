8bfaaae05a1c4b1ba16b019a666e2f1db65f4de9
Abhigna Chintala <23211a1233@bvrit.ac.in>
2026-01-22 15:40:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    document.querySelectorAll("section").forEach(section => {
+        let position = section.getBoundingClientRect().top;
+        if (position < window.innerHeight - 100) {
+            section.style.opacity = 1;
+            section.style.transform = "translateY(0)";
+        }
+    });
+});
